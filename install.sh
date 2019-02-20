4da03fdd7f069eb24bbe21546634901225c45bb5
Supermanuu <manuel.pascual@caseonit.com>
2019-02-20 11:42:23 +0100

    Fixed . .. .git syncronization.

--- install.sh
+++ install.sh
@@ -2,4 +2,4 @@
 
 INSTALLATION_PATH=/home/manu/.config/StarUML/extensions/user/staruml.cplus
 
-rsync -av --progress --exclude="install.sh" . $INSTALLATION_PATH
+rsync -av --progress --exclude="install.sh" --exclude=".git" --exclude="." --exclude=".." . $INSTALLATION_PATH
