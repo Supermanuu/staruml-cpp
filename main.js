167d67db417c0e123a54c1a4ed1e867787d163f2
Supermanuu <manuel.pascual@caseonit.com>
2019-02-20 13:23:05 +0100

    Added path remembering.

--- main.js
+++ main.js
@@ -44,72 +44,75 @@ function getRevOptions () {
   }
 }
 
+var base
+var path
+var genOptions
+var revOptions
+
 /**
  * Command Handler for C++ Generate
- *
- * @param {Element} base
- * @param {string} path
- * @param {Object} options
  */
-function _handleGenerate (base, path, options) {
-  // If options is not passed, get from preference
-  options = options || getGenOptions()
-  // If base is not assigned, popup ElementPicker
-  if (!base) {
-    app.elementPickerDialog.showDialog('Select a base model to generate codes', null, type.UMLPackage).then(function ({buttonId, returnValue}) {
-      if (buttonId === 'ok') {
-        base = returnValue
-        // If path is not assigned, popup Open Dialog to select a folder
-        if (!path) {
-          var files = app.dialogs.showOpenDialog('Select a folder where generated codes to be located', null, null, { properties: [ 'openDirectory' ] })
-          if (files && files.length > 0) {
-            path = files[0]
-            codeGenerator.generate(base, path, options)
-          }
-        } else {
-          codeGenerator.generate(base, path, options)
-        }
-      }
-    })
-  } else {
+function _handleGenerate () {
+  // If genOptions is not passed, get from preference
+  genOptions = genOptions || getGenOptions()
+
+  var setPathAndGenerate = () => {
     // If path is not assigned, popup Open Dialog to select a folder
     if (!path) {
       var files = app.dialogs.showOpenDialog('Select a folder where generated codes to be located', null, null, { properties: [ 'openDirectory' ] })
       if (files && files.length > 0) {
         path = files[0]
-        codeGenerator.generate(base, path, options)
+        codeGenerator.generate(base, path, genOptions)
       }
     } else {
-      codeGenerator.generate(base, path, options)
+      codeGenerator.generate(base, path, genOptions)
     }
+  }
+
+  // If base is not assigned, popup ElementPicker
+  if (!base) {
+    app.elementPickerDialog.showDialog('Select a base model to generate codes', null, type.UMLPackage).then(function ({buttonId, returnValue}) {
+      if (buttonId === 'ok') {
+        base = returnValue
+        setPathAndGenerate ()
+      }
+    })
+  } else {
+    setPathAndGenerate ()
   }
 }
 
 /**
- * Command Handler for C++ Reverse
- *
- * @param {string} basePath
- * @param {Object} options
+ * Clear paths
  */
-function _handleReverse (basePath, options) {
-  // If options is not passed, get from preference
-  options = getRevOptions()
-  // If basePath is not assigned, popup Open Dialog to select a folder
-  if (!basePath) {
-    var files = app.dialogs.showOpenDialog('Select Folder', null, null, { properties: [ 'openDirectory' ] })
-    if (files && files.length > 0) {
-      basePath = files[0]
-      codeAnalyzer.analyze(basePath, options)
-    }
+function _handlePaths () {
+  base = null
+  path = null
+}
+
+/**
+ * Command Handler for C++ Reverse
+ */
+function _handleReverse () {
+  var basePath
+  // If revOptions is not passed, get from preference
+  revOptions = revOptions || getRevOptions()
+  var files = app.dialogs.showOpenDialog('Select Folder', null, null, { properties: [ 'openDirectory' ] })
+  if (files && files.length > 0) {
+    basePath = files[0]
+    codeAnalyzer.analyze(basePath, revOptions)
   }
 }
 
 function _handleConfigure () {
   app.commands.execute('application:preferences', 'cpp')
+  genOptions = getGenOptions()
+  revOptions = getRevOptions()
 }
 
 function init () {
   app.commands.register('cpp:generate', _handleGenerate)
+  app.commands.register('cpp:paths', _handlePaths)
   app.commands.register('cpp:reverse', _handleReverse)
   app.commands.register('cpp:configure', _handleConfigure)
 }
